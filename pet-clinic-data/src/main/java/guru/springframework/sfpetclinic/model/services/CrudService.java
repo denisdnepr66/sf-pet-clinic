6dc35c138755fe71bc4a904253fd985394684761
denisdnepr66 <56921743+denisdnepr66@users.noreply.github.com>
2021-03-24 13:11:55 +0200

    impl map based services. Closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/model/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/model/services/CrudService.java
@@ -1,19 +1,16 @@
 package guru.springframework.sfpetclinic.model.services;
 
-import org.springframework.data.annotation.Id;
-
 import java.util.Set;
 
 public interface CrudService<T, ID> {
 
     Set<T> findAll();
 
-    T findById(Id id);
+    T findById(ID id);
 
     T save(T object);
 
     void delete(T object);
 
-    void deleteById(Id id);
-
+    void deleteById(ID id);
 }
